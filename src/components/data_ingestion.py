71fbb35da412096457ebe466991920bc164e91ce
Sagar3195 <saggyd95@gmail.com>
2023-05-31 10:28:17 +0530

    EDA

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass 
+
+#Initialize the data ingestion configuration
